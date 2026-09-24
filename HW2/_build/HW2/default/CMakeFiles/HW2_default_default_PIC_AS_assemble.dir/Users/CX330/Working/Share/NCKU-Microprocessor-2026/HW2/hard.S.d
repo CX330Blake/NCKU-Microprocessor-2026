06fd42219eb06bc58a7b264ae36da954602a9eea
CX330 <cx330tw@gmail.com>
2026-09-24 12:10:19 +0800

    update testcases after demo

---
+++ HW2/_build/HW2/default/CMakeFiles/HW2_default_default_PIC_AS_assemble.dir/Users/CX330/Working/Share/NCKU-Microprocessor-2026/HW2/hard.S.d
@@ -0,0 +1,2 @@
+CMakeFiles/HW2_default_default_PIC_AS_assemble.dir/Users/CX330/Working/Share/NCKU-Microprocessor-2026/HW2/hard.S.i: \
+  /Users/CX330/Working/Share/NCKU-Microprocessor-2026/HW2/hard.S
